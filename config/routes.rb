187443cac065929cb6539f852892b415f3bd0ac8
Viet Le <lethanhviet.bay@gmail.com>
2019-01-03 10:29:59 +0700

    Define association between movie and user model. Update action movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users,
-              path: '',
-              path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout', edit: 'profile' }
+  devise_for :users
+              # path: '',
+              # path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout', edit: 'profile' }
   resources :movies
   root 'pages#home'
 end
