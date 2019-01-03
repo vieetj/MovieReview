049510a9dd643501baf8dfdf4e016ec6525367b5
Viet Le <lethanhviet.bay@gmail.com>
2019-01-03 16:02:00 +0700

    Change name of url when login, logout, register

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  devise_for :users
-              # path: '',
-              # path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout', edit: 'profile' }
+  devise_for :users,
+              path: '',
+              path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout', edit: 'profile' }
   resources :movies do
     resources :reviews, except: [:show, :index]
   end
